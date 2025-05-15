46e7ffa3c3e52446f5b2b197edf4369e3e1c14f5
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-05-15 22:23:41 +0530

    minor update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: ['font-mova'],
   theme: {
     extend: {
       fontFamily: {
-        mova: ['Mova'],
+        amsterdam: ['Amsterdam', 'sans-serif'],
+        arenq: ['Arenq', 'sans-serif'],
+        beckman: ['Beckman', 'sans-serif'],
+        cinderela: ['Cinderela', 'sans-serif'],
+        countryside: ['Countryside', 'sans-serif'],
+        countrysideItalic: ['Countryside Italic', 'sans-serif'],
+        darlington: ['Darlington', 'sans-serif'],
+        halimun: ['Halimun', 'sans-serif'],
+        migae: ['Migae', 'sans-serif'],
+        mova: ['Mova', 'sans-serif'],
+        overcame: ['Overcame', 'sans-serif'],
+        overcameOutline: ['Overcame Outline', 'sans-serif'],
+        lukyfiend: ['Lukyfiend', 'sans-serif'],
       },
     },
   },
   plugins: [],
-};
+}
